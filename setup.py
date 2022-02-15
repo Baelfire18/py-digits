45da2a13e7d2fdad1c226d20bd9971d77d7e0b34
Baelfire18 <jacastro18@uc.cl>
2022-02-15 18:11:33 -0300

    feat(v0.1.3): release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='pi-digits',
-      version='0.1.2',
+      version='0.1.3',
       description='Sum the first X Pi digits',
       url='https://github.com/Baelfire18/pi-digits',
       author='Jose Antonio Castro',
